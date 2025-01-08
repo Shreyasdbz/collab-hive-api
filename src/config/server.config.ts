9b724aecc0498b0e4d861fed215fc412941e9ca3
Shreyas Sane <shreyasdbz@gmail.com>
2025-01-08 17:43:59 -0500

    default port

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const configs: TServerConfig = {
     host: env.get('HOST').asString() || 'http://localhost',
-    port: env.get('PORT').asInt() || 3000,
+    port: env.get('PORT').asInt() || 8080,
     log_level: env.get('LOG_LEVEL').asString() || 'tiny',
     db_url: env.get('DB_URL').asString() || '',
 };
